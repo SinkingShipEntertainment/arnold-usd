95c518c9d2b6ccfc6350c1675e7deebd9efe0acb
Marcelo Sercheli <msercheli@gmail.com>
2021-09-01 14:47:35 -0400

    Bump version of plugin and SDK

--- package.py
+++ package.py
@@ -39,9 +39,9 @@ private_build_requires = [
 
 variants = [
     ["platform-linux", "arch-x86_64", "os-centos-7", "boost-1.61", "usd-20.08.sse.1", "!ptex"],
-    #["platform-linux", "arch-x86_64", "os-centos-7", "boost-1.70", "usd-20.08.sse.1", "!ptex"],  # Not available because of Boost
+    #["platform-linux", "arch-x86_64", "os-centos-7", "boost-1.70", "usd-20.08.sse.1", "!ptex"],  # Not available because of Boost/Python 3
     #["platform-linux", "arch-x86_64", "os-centos-7", "boost-1.61", "usd-21.05.sse.1", "!ptex"],  # Not available because of MaterialX
-    #["platform-linux", "arch-x86_64", "os-centos-7", "boost-1.70", "usd-21.05.sse.1", "!ptex"],  # Not available because of MaterialX and Boost
+    #["platform-linux", "arch-x86_64", "os-centos-7", "boost-1.70", "usd-21.05.sse.1", "!ptex"],  # Not available because of MaterialX and Boost/Python 3
 ]
 
 # Pass cmake arguments:
@@ -74,10 +74,3 @@ def commands():
     env.ARNOLD_PLUGIN_PATH.append("{root}/procedural")
     env.PXR_PLUGINPATH_NAME.append("{root}/plugin")
     env.PATH.append("{root}/bin")
-
-    # NOTE: Old version of the source code was creating "lib"
-    #env.PYTHONPATH.append("{root}/lib/python")
-    #env.PXR_PLUGINPATH_NAME.append("{root}/lib/usd")
-    #env.LD_LIBRARY_PATH.append("{root}/lib")
-
-    #alias("usdview", "LD_PRELOAD=$REZ_ARNOLD_SDK_ROOT/bin/libai.so usdview")
